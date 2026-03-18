321b14f80d33b0cd460737697d5f79fcf0478bec
franckel777 <franckel9nathan@gmail.com>
2026-03-18 16:43:18 +0100

    Structuration du projet

---
+++ src/app/Main.java
@@ -0,0 +1,27 @@
+package app;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int option = 0;
+		Scanner sc = new Scanner(System.in);
+		while (option != 5) {
+			System.out.println("=== GESTION DES TACHES ===");
+			System.out.println("1.Ajouter une tache");
+			System.out.println("2.Afficher les taches");
+			System.out.println("3.Modifier le statut d'une tache");
+			System.out.println("4.Supprimer une tache");
+			System.out.println("5.Quitter");
+			System.out.println("Votre choix");
+			try {
+				int choix = sc.nextInt();
+				sc.nextLine();
+			} catch (Exception e) {
+				System.out.println("Entrez un choix valide!");
+			}
+		}
+	}
+
+}
---
+++ src/model/Tache.java
@@ -0,0 +1,59 @@
+package model;
+
+public class Tache {
+	private int id;
+	private String titre;
+	private String description;
+	private String responsable;
+	private String statut;
+
+	public Tache(int id, String titre, String description, String responsable, String statut) {
+		this.id = id;
+		this.titre = titre;
+		this.description = description;
+		this.responsable = responsable;
+		this.statut = statut;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getResponsable() {
+		return responsable;
+	}
+
+	public void setResponsable(String responsable) {
+		this.responsable = responsable;
+	}
+
+	public String getStatut() {
+		return statut;
+	}
+
+	public void setStatut(String statut) {
+		this.statut = statut;
+	}
+	
+	
+}
