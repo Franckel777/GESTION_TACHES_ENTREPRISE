5901ad7f25fb45b878c397b02872b2da5ad7d80a
franckel777 <franckel9nathan@gmail.com>
2026-03-19 15:35:10 +0100

    Ajout de l'interface DAO et de la classe qui l'implemente

--- src/dao/TacheDAO.java
+++ src/dao/TacheDAO.java
@@ -1,5 +1,16 @@
 package dao;
 
-public class TacheDAO {
-
+import java.sql.*;
+import java.util.*;
+public interface TacheDAO<Tache> {
+	
+	Tache get(int id) throws SQLException;
+	
+	List<Tache> afficherTache() throws SQLException;
+	
+	int ajouterTache() throws SQLException;
+	
+	int modifierTache() throws SQLException;
+	
+	int supprimerTache() throws SQLException;
 }
---
+++ src/dao/TacheDAOImpl.java
@@ -0,0 +1,5 @@
+package dao;
+
+public class TacheDAOImpl implements TacheDAO {
+
+}
